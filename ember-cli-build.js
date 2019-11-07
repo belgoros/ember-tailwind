0fd1dee9ce40640b73e7336ba057a6d9fe6622b2
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-11-07 16:38:00 +0100

    Fix responsive power-select element issue
    - remove setting the theme to false in ember-cli-build.js
    - apply styles to the outer div to make EPS responsive
    - change sm classes to md to hide Logo

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    'ember-power-select': {
-      theme: false
-    },
     postcssOptions: {
       compile: {
         plugins: [
