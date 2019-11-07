5f992af4a6f9f9a44b0959cec09b58d105eaf384
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-11-07 17:19:55 +0100

    [dkt] Display shops in EPS, refactor user/role section

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,12 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  cities: null,
-  destination: null,
+  shops: null,
+  current: null,
 
   init() {
     this._super(...arguments);
-    this.set('destination', 'Missouri');
-    this.set('cities', ['New Mexico', 'Missouri', 'Texas', 'Lorem ipsum dolor sit amet consectetur adipisicing elit']);
+    this.set('current', '101-London');
+    this.set('shops', ['101-London', '1-Paris', '648-Villeneuve-D\'ascq', '1256-Lorem ipsum dolor sit amet consectetur adipisicing elit']);
   }
 });
